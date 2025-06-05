f24d54ac1147bf2342cc3e428db24aeadec8daf5
KaizokuGari <82303991+KaizokuGari@users.noreply.github.com>
2025-06-05 14:51:22 +0200

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,8 @@ output "key_ssm_param" {
 output "public_ip" {
   value = aws_instance.demo_instance.public_ip
 }
+
+output "private_key_pem" {
+  value     = tls_private_key.demo_key.private_key_pem
+  sensitive = true
+}
